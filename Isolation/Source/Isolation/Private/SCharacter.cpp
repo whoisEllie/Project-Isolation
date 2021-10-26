20e3c78d96bd68e6fbfa1ee2f7631c277c964460
whoIsEllie <lukaskelemen@gmail.com>
2021-10-26 20:26:29 +0200

    Testing again, fingers crossed!

--- Isolation/Source/Isolation/Private/SCharacter.cpp
+++ Isolation/Source/Isolation/Private/SCharacter.cpp
@@ -1,6 +1,5 @@
 // Fill out your copyright notice in the Description page of Project Settings.
-// This is just a test
-
+//Testing again, just to make sure this bad boi is working >:D
 
 
 #include "SCharacter.h"
