7e9a7854c2619bc8c085cb2b038e5cf4ba6a7c1d
Ellie Kelemen <37041249+whoisEllie@users.noreply.github.com>
2022-10-03 20:28:32 +0100

    Removing old files

--- Isolation/Source/Isolation/Isolation.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Isolation.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Isolation, "Isolation" );
